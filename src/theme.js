c6f6d0b3ec87c05226a3acf5eee92bcd747a5aa3
BeeGeeEss <brando.g.smith@gmail.com>
2026-06-16 22:00:10 +1000

    FEAT: Theme

--- src/theme.js
+++ src/theme.js
@@ -1,33 +1,32 @@
 import { createTheme } from "@mui/material/styles";
 
+const headingStyle = {
+  fontFamily: '"Roboto", sans-serif',
+  fontWeight: 500,
+};
+
 const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#8C52FF",
+    },
+    secondary: {
+      main: "#7ED957",
+    },
+    background: {
+      default: "#F2FFE2",
+    },
+  },
+
   typography: {
     fontFamily: '"Roboto Condensed", sans-serif',
 
-    h1: {
-      fontFamily: '"Roboto", sans-serif',
-      fontWeight: 500,
-    },
-    h2: {
-      fontFamily: '"Roboto", sans-serif',
-      fontWeight: 500,
-    },
-    h3: {
-      fontFamily: '"Roboto", sans-serif',
-      fontWeight: 500,
-    },
-    h4: {
-      fontFamily: '"Roboto", sans-serif',
-      fontWeight: 500,
-    },
-    h5: {
-      fontFamily: '"Roboto", sans-serif',
-      fontWeight: 500,
-    },
-    h6: {
-      fontFamily: '"Roboto", sans-serif',
-      fontWeight: 500,
-    },
+    h1: headingStyle,
+    h2: headingStyle,
+    h3: headingStyle,
+    h4: headingStyle,
+    h5: headingStyle,
+    h6: headingStyle,
   },
 });
 
